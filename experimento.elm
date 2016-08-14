e6b9cc332c71260fe068b5f12461f05aa6314f88
Ivan Acosta-Rubio <ivan@Nabuko.local>
2016-08-14 10:28:54 -0500

    💧 Sample project with Messages and some Randomness

---
+++ experimento.elm
@@ -0,0 +1,95 @@
+module Main exposing (..)
+
+import Html exposing (Html, div, text)
+import Html.App as App
+import Window exposing (..)
+import Mouse
+import Keyboard exposing (..)
+import Char exposing (fromCode)
+import Task exposing (..)
+import Basics.Extra exposing (..)
+import Random exposing (..)
+
+
+main : Program Never
+main =
+    App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+type alias Model =
+    { windowSize : Window.Size
+    , mouse : Point
+    , keyboard : String
+    , points : List Point
+    }
+
+
+type alias Point =
+    { x : Int, y : Int }
+
+
+initialModel : Model
+initialModel =
+    { windowSize = { height = 0, width = 0 }
+    , mouse = { x = 0, y = 0 }
+    , keyboard = ""
+    , points = []
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initialModel, initialWindowSize )
+
+
+randomPoint : Cmd Msg
+randomPoint =
+    Random.generate RandomPointMsg (Random.int 0 100)
+
+
+initialWindowSize : Cmd Msg
+initialWindowSize =
+    Task.perform never WindMsg Window.size
+
+
+type Msg
+    = KeyMsg Keyboard.KeyCode
+    | MouseMsg Mouse.Position
+    | WindMsg Window.Size
+    | RandomPointMsg Int
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        MouseMsg position ->
+            ( { model | mouse = position }, Cmd.none )
+
+        KeyMsg code ->
+            ( { model | keyboard = (fromCode code) |> toString }, Cmd.none )
+
+        WindMsg windowSize ->
+            ( { model | windowSize = windowSize }, Cmd.none )
+
+        RandomPointMsg int ->
+            ( { model | points = List.append model.points [ { x = int, y = int } ] }, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ Mouse.clicks MouseMsg
+        , Keyboard.presses KeyMsg
+        , Window.resizes WindMsg
+        ]
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ Html.text (toString model) ]
